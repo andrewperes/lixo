434f8506335326e40e6ca8852550ca9cb2420114
andrewperes <63035618+andrewperes@users.noreply.github.com>
2023-10-25 21:43:49 -0300

    Add files via upload

--- agenda/app/model/Servico.php
+++ agenda/app/model/Servico.php
@@ -7,12 +7,24 @@ class Servico
     private $tenpo;
     private $valor;
     private $logo;
-    
 
+    // Metodo para cadastar um serviço
+    public function cadastrar() 
+    {
 
+    }
 
+    // metodo para atualizar um serviço
+    public function atualizar()
+    {
 
+    }
 
+    // metodo para apagar um serviço
+    public function apagar()
+    {
+
+    }
 }
 
 
